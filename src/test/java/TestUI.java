b08acfae46811ca28357b53e20d4010c19ef8492
SmthInYourEyes <smthinyoureyes@hotmail.com>
2023-12-10 16:57:57 +0300

    Тесты для открытия всех страниц

--- src/test/java/TestUI.java
+++ src/test/java/TestUI.java
@@ -11,14 +11,13 @@ import static java.lang.Boolean.TRUE;
 
 public class TestUI {
    SelenideElement news = $x("//*[@id='floatmenu']/ul/li[1]/a");
-    SelenideElement aboutProgram = $x("//*[@id='floatmenu']/ul/li[2]/a/span");
+    SelenideElement aboutProgram = $x("//*[@id='floatmenu']/ul/li[2]/a");
     SelenideElement feedback = $x("//*[@id='floatmenu']/ul/li[3]/a");
     SelenideElement articles = $x("//*[@id='floatmenu']/ul/li[4]/a");
     SelenideElement conditions = $x("//*[@id='floatmenu']/ul/li[5]/a");
     SelenideElement contacts = $x("//*[@id='floatmenu']/ul/li[6]/a");
     SelenideElement statusPanel = $x("//*[@id=\"status-panel\text()]");
 
-    private final String url = "https://www.simbirsoft.com/contacts";
 
 @Test
     public void openAboutProgram()  {
@@ -26,6 +25,40 @@ public class TestUI {
     Configuration.browserSize ="1920x1080";
         Selenide.open("https://sitechco.ru/");
         element(aboutProgram).click();
-
+    }
+    @Test
+    public void openNews()  {
+        Configuration.holdBrowserOpen = TRUE;
+        Configuration.browserSize ="1920x1080";
+        Selenide.open("https://sitechco.ru/");
+        element(aboutProgram).click();
+    }
+    @Test
+    public void openFeedback()  {
+        Configuration.holdBrowserOpen = TRUE;
+        Configuration.browserSize ="1920x1080";
+        Selenide.open("https://sitechco.ru/");
+        element(aboutProgram).click();
+    }
+    @Test
+    public void openArticles()  {
+        Configuration.holdBrowserOpen = TRUE;
+        Configuration.browserSize ="1920x1080";
+        Selenide.open("https://sitechco.ru/");
+        element(aboutProgram).click();
+    }
+    @Test
+    public void openConditions()  {
+        Configuration.holdBrowserOpen = TRUE;
+        Configuration.browserSize ="1920x1080";
+        Selenide.open("https://sitechco.ru/");
+        element(aboutProgram).click();
+    }
+    @Test
+    public void openContacts()  {
+        Configuration.holdBrowserOpen = TRUE;
+        Configuration.browserSize ="1920x1080";
+        Selenide.open("https://sitechco.ru/");
+        element(aboutProgram).click();
     }
 }
