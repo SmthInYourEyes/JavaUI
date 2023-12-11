19cc4a9b16d815075dd0947e5af8fd1de8daf10e
SmthInYourEyes <smthinyoureyes@hotmail.com>
2023-12-11 19:35:01 +0300

    Тесты от всех страниц
    Отлажены и запущены с параметрами

--- src/test/java/TestUI.java
+++ src/test/java/TestUI.java
@@ -1,64 +1,59 @@
-
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static java.lang.Boolean.TRUE;
 
 
 
 public class TestUI {
-   SelenideElement news = $x("//*[@id='floatmenu']/ul/li[1]/a");
-    SelenideElement aboutProgram = $x("//*[@id='floatmenu']/ul/li[2]/a");
-    SelenideElement feedback = $x("//*[@id='floatmenu']/ul/li[3]/a");
-    SelenideElement articles = $x("//*[@id='floatmenu']/ul/li[4]/a");
-    SelenideElement conditions = $x("//*[@id='floatmenu']/ul/li[5]/a");
-    SelenideElement contacts = $x("//*[@id='floatmenu']/ul/li[6]/a");
-    SelenideElement statusPanel = $x("//*[@id=\"status-panel\text()]");
+    SelenideElement news = $x("//div[@id='floatmenu']/ul/li[1]/a");
+    SelenideElement aboutProgram = $x("//div[@id='floatmenu']/ul/li[2]/a");
+    SelenideElement feedback = $x("//div[@id='floatmenu']/ul/li[3]/a");
+    SelenideElement articles = $x("//div[@id='floatmenu']/ul/li[4]/a");
+    SelenideElement conditions = $x("//div[@id='floatmenu']/ul/li[5]/a");
+    SelenideElement contacts = $x("//div[@id='floatmenu']/ul/li[6]/a");
+    SelenideElement statusPanel = $x("//div[@id='status-panel']");
 
-
-@Test
-    public void openAboutProgram()  {
-    Configuration.holdBrowserOpen = TRUE;
-    Configuration.browserSize ="1920x1080";
-        Selenide.open("https://sitechco.ru/");
-        element(aboutProgram).click();
-    }
     @Test
-    public void openNews()  {
-        Configuration.holdBrowserOpen = TRUE;
-        Configuration.browserSize ="1920x1080";
-        Selenide.open("https://sitechco.ru/");
-        element(aboutProgram).click();
+    public void openNews() {
+        open("https://sitechco.ru/");
+        element(news).click();
+        element(statusPanel).shouldHave(text("Sitechco.ru"));
     }
+
+    @Test
+    public void openAboutProgram()  {
+        open("https://sitechco.ru/");
+        element(aboutProgram).click();
+        element(statusPanel).shouldHave(text("О Программе"));
+    }
+
     @Test
     public void openFeedback()  {
-        Configuration.holdBrowserOpen = TRUE;
-        Configuration.browserSize ="1920x1080";
-        Selenide.open("https://sitechco.ru/");
-        element(aboutProgram).click();
+        open("https://sitechco.ru/");
+        element(feedback).click();
+        element(statusPanel).shouldHave(text("Отзывы"));
     }
+
     @Test
     public void openArticles()  {
-        Configuration.holdBrowserOpen = TRUE;
-        Configuration.browserSize ="1920x1080";
-        Selenide.open("https://sitechco.ru/");
-        element(aboutProgram).click();
+        open("https://sitechco.ru/");
+        element(articles).click();
+        element(statusPanel).shouldHave(text("Статьи"));
     }
+
     @Test
     public void openConditions()  {
-        Configuration.holdBrowserOpen = TRUE;
-        Configuration.browserSize ="1920x1080";
-        Selenide.open("https://sitechco.ru/");
-        element(aboutProgram).click();
+        open("https://sitechco.ru/");
+        element(conditions).click();
+        element(statusPanel).shouldHave(text("Условия"));
     }
+
     @Test
     public void openContacts()  {
-        Configuration.holdBrowserOpen = TRUE;
-        Configuration.browserSize ="1920x1080";
-        Selenide.open("https://sitechco.ru/");
-        element(aboutProgram).click();
+        open("https://sitechco.ru/");
+        element(contacts).click();
+        element(statusPanel).shouldHave(text("Контакты"));
     }
 }
